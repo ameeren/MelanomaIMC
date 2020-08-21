4206e9adedb098ad9db26b17b4b684792885236f
SchulzDan <shooltz@gmx.de>
2020-08-21 14:48:24 +0000

    fixed fraction helper functions and scripts

--- code/helper_functions/plotCellFracGroupsSubset.R
+++ code/helper_functions/plotCellFracGroupsSubset.R
@@ -1,7 +1,7 @@
 # function to plot (boxplot) the fraction of each celltype for all images in an single cell experiment.
 # fractions can be colored according to Image level colData in the single cell experiment
 
-plotCellFracGroupsSubset <- function(x,CellClass,colour_by, celltype_subset,cluster_col) {
+plotCellFracGroupsSubset <- function(x,CellClass,split_by, celltype_subset,cluster_col) {
   # check if x is SingleCellExperiment
   .sceCheck(x)
 
@@ -14,9 +14,10 @@ plotCellFracGroupsSubset <- function(x,CellClass,colour_by, celltype_subset,clus
   if (is.null(cluster_col)) {
     stop("Provide the name of the celltype cluster column from the object")
   }
-  if (is.null(color_by)) {
-    stop("Provide metadata entry from the colData which you want to group by")
+  if (is.null(split_by)) {
+    stop("Provide the name of the celltype cluster column from the object")
   }
+
 
 
   # check if selected variables exist
@@ -26,16 +27,13 @@ plotCellFracGroupsSubset <- function(x,CellClass,colour_by, celltype_subset,clus
       stop("The entry for CellClass is not a colData slot of the object.")
     }
   }
-
-  # check if color_by does not exceed the number of images
-  if (! is.null(color_by)) {
-    if (! color_by %in% entries) {
-      stop("The entry for color_by is not a colData slot of the object.")
+  if (! is.null(split_by)) {
+    if (! split_by %in% entries) {
+      stop("The entry for spllit_by is not a colData slot of the object.")
     }
-    if (length(unique(colData(x)[,color_by])) > length(unique(x$ImageNumber))) {
-      stop("Number of colors selected not supported")
-      }
   }
+
+
 
 
   cur_df <- data.frame(ImageNumber = (colData(x)[,"ImageNumber"]))
@@ -44,17 +42,23 @@ plotCellFracGroupsSubset <- function(x,CellClass,colour_by, celltype_subset,clus
 
   #cur_df$colour_by <- as.factor(colData(x)[,colour_by])
 
-  cur_df$cluster <- (colData(x)[,celltype_cluster_col])
+  cur_df$cluster <- (colData(x)[,cluster_col])
 
   cur_df <- dcast(cur_df,formula = " ImageNumber + split_by ~ cluster",fun.aggregate = length)
 
   cur_df <- melt(cur_df,id.vars = c("ImageNumber","split_by"))
 
-  if(scale == "count"){
+  cur_df <- cur_df %>%
+    group_by(ImageNumber) %>%
+    mutate(total = sum(value), frac = value/total) %>%
+    separate(col = variable, into = c("celltype","cluster"),remove = FALSE,sep = "_")
+
+  if(! celltype_subset %in% cur_df$celltype){
+    stop("provide a celltype subset which is a valid split of the clusters")
+  }
+
     cur_df %>%
       group_by(ImageNumber) %>%
-      mutate(total = sum(value), frac = value/total) %>%
-      separate(col = variable, into = c("celltype","cluster"),remove = FALSE,sep = "_") %>%
       filter(celltype == celltype_subset) %>%
       ggplot(aes(x=variable,y=frac,fill=split_by))+
       geom_boxplot(outlier.shape = NA,position=position_dodge(width=1))+
@@ -62,7 +66,6 @@ plotCellFracGroupsSubset <- function(x,CellClass,colour_by, celltype_subset,clus
       theme_bw()+
       theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
       ylab("Fraction of celltype per Image")+
-      scale_fill_discrete(name = colour_by)
-  }
+      scale_fill_discrete(name = split_by)
 
 }
